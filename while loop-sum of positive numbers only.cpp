1a5cd725343a26b6b58b4d74c44969c30bd2a24b
Xexvil001 <114426469+Xexvil001@users.noreply.github.com>
2022-10-13 13:25:55 +0300

    Add files via upload

---
+++ while loop-sum of positive numbers only.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main () {
+int number=0;
+int sum =0;
+//take input from the user
+cout <<"Enter a number:";
+cin >> number;
+while(number >= 0){
+// add all positive numbers 
+sum += number;
+// take input again if the number is positive 
+cout <<"Enter a number:";
+cin>>number;
+}
+// display the sum
+cout <<"\nThe sum is "<< sum << endl;
+return 0;
+}
