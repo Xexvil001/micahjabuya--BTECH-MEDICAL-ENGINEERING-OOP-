56c8a56dde05eb0c8b33a8749b7065adb3ed5585
Xexvil001 <114426469+Xexvil001@users.noreply.github.com>
2022-11-16 12:54:13 +0300

    Add files via upload

---
+++ inheritance[1].cpp
@@ -0,0 +1,30 @@
+//C++ program to demonstrate inheritance
+#include <iostream>
+using namespace std;
+// base class
+class Father {
+ public:
+ void villa() {
+ cout<<"I own villa!"<< endl;
+ }
+ void land() {
+ cout<<"I own land!"<< endl;
+ }
+};
+// derived class
+class Son : public Father {
+ public:
+ void rangerover() {
+ cout<<"I can buy ranger over!"<< endl;
+ }
+};
+int main() {
+// create object of the Son class
+ Son son1;
+ // Calling members of the base class
+ son1.villa();
+ son1.land();
+ //Calling member of the derived class
+ son1.rangerover();
+ return 0;
+}
