a889f6df1544b3b6a9a7a638e00948f4302d9eda
Xexvil001 <114426469+Xexvil001@users.noreply.github.com>
2022-11-16 15:20:15 +0300

    Add files via upload

---
+++ 3DArray.cpp
@@ -0,0 +1,16 @@
+// C++ Program to Store value entered by user in
+// three dimensional array and display it.
+#include <iostream>
+using namespace std;
+int main () {
+int i, j, k;
+int a[2][3][3]={{{1,2,3},{4,5,6},{7,8,9}},{{10,11,12},{13,14,15},
+{16,17,18}}};
+for(i=0;i<2;i++)
+for(j=0;j<3;j++)
+for(k=0;k<3;++k)
+{
+cout<<"a["<<i<<"]["<<j<<"]["<<k<<"]="<<a[i][j][k]<<endl;
+}
+return 0;
+}
